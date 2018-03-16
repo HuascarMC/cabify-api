a23d768e7dd73390024500b4c1929dc7cd0a9d8a
Huascar <huascarmc@hotmail.com>
2018-03-16 21:26:53 +0000

    models set up, tests passed

--- app/models/cab.rb
+++ app/models/cab.rb
@@ -1,2 +1,6 @@
 class Cab < ApplicationRecord
+
+ has_one :location, dependent: :destroy
+
+ validates_presence_of :state, :name, :city
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,5 @@
 class Location < ApplicationRecord
   belongs_to :cab
+
+  validates_presence_of :lon, :lat
 end
